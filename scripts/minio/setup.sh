c62a54f962f2d8e9b4541b5006573cc85121b140
Henry Nguyen <henryng.tech.dev@gmail.com>
2026-07-14 16:44:04 +0700

    Add multi-replica deployment for docker compose, allowing load balancing across different replicas of services

--- scripts/minio/setup.sh
+++ scripts/minio/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 mc alias set myminio http://${MINIO_INTERNAL_ENDPOINT} ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY};
-mc mb myminio/${MINIO_BUCKET};
-mc anonymous set public myminio/${MINIO_BUCKET};
+
+# Create bucket (idempotent by, if exist, just returns as true instead)
+mc mb myminio/${MINIO_BUCKET} || true;
+
+# Set public access (idempotent by, if exist, just returns as true instead)
+mc anonymous set public myminio/${MINIO_BUCKET} || true;
+
+echo "MinIO setup completed!"
+
 exit 0;
